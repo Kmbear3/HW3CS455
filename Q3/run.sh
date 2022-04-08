2a222e57670a125311858743a4573d496f6ffacc
Kass <kmbarram@gmail.com>
2022-04-08 14:27:56 -0600

    Output values look correct, still need to validate

--- Q3/run.sh
+++ Q3/run.sh
@@ -2,4 +2,4 @@ clear
 
 gradle build
 
-$HADOOP_HOME/bin/hadoop jar build/libs/Q3-1.0-SNAPSHOT.jar Question3 /wc /q3_0
+$HADOOP_HOME/bin/hadoop jar build/libs/Q3-1.0-SNAPSHOT.jar Question3 /aqi /q3_8
--- Q3/src/main/java/Question3.java
+++ Q3/src/main/java/Question3.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.mapreduce.Job;
@@ -17,46 +19,91 @@ import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.*;
 import java.util.Collections;
+import java.text.SimpleDateFormat;
 
 
 
 
 public class Question3 {
-	public static class MapperQ3 extends Mapper<Object, Text, Text, NullWritable> {
+	public static class MapperQ3 extends Mapper<Object, Text, Text, IntWritable> {
 
         @Override
         protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            while (itr.hasMoreTokens()) {
-                String s = itr.nextToken();
-                Text node = new Text( s );
-                context.write(node, NullWritable.get()); 
+            String[] line = value.toString().split(",");
+            String desiredYear = "2020";
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy");
+            String year = sdf.format(Long.parseLong(line[2]));
+
+            if(year.equals(desiredYear)){
+                context.write(new Text(line[0]), new IntWritable(Integer.parseInt(line[1])));
             }
+            
                 
         }
 	
     }
-
-
-
-    public static class ReducerQ3 extends Reducer<Text, NullWritable, IntWritable, NullWritable>{
-
-        private Set<String> distinctNodes ; 
+    public static class NodeComparator implements Comparator<Map.Entry<Text, Double>>{
 
         @Override
-        protected void setup(Context context) {
-            distinctNodes = new HashSet<String>();
+        public int compare(Map.Entry<Text, Double> node1, Map.Entry<Text, Double> node2){
+    
+                int compareValue = node1.getValue().compareTo(node2.getValue());
+                if(compareValue > 0){ // was == 1
+                    return 1;
+                }
+                else if(compareValue == 0){  
+                    int compareKey =  node1.getKey().toString().compareTo(node2.getKey().toString()); 
+                    if(compareKey > 0){ //was == 1
+                        return -1;
+                    }
+                    else if(compareKey < 0){ // was == -1
+                        return 1;
+                    }
+                    else{
+                        return 0;
+                    }
+                }
+                else if(compareValue < 0){ // was == -1
+                    return -1;
+                }
+                else{
+                    return 0; 
+                }
         }
     
+    }
+
+
+    public static class ReducerQ3 extends Reducer<Text, IntWritable, Text, DoubleWritable>{
+        private List<Map.Entry<Text, Double>> highestAverage = new ArrayList<Map.Entry<Text, Double>>();
+
         @Override
-        protected void reduce(Text key, Iterable<NullWritable> values, Context context) {
-            distinctNodes.add(key.toString());
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            long sum = 0;
+            int length = 0;
+            for(IntWritable x : values){
+                sum += x.get();
+                length++;
+            }
+            double average = (double)sum/length;
+
+            highestAverage.add(new AbstractMap.SimpleEntry<Text, Double>(new Text(key), average));
+            Collections.sort(highestAverage, new  NodeComparator());
+
+            if(highestAverage.size() > 10){
+                highestAverage.remove(highestAverage.size() - 1);
+            }
         }
     
-        @Override
+       @Override
         protected void cleanup(Context context) throws IOException, InterruptedException {
-            IntWritable noOfNodes = new IntWritable(distinctNodes.size());
-            context.write(noOfNodes, NullWritable.get()); 
+            for(Map.Entry<Text, Double> entry : highestAverage){
+                Text county = new Text(entry.getKey());
+                DoubleWritable average = new DoubleWritable(entry.getValue());
+                context.write(county, average);
+            }
+
         }
 
     }
@@ -71,9 +118,9 @@ public class Question3 {
 		job.setReducerClass(Question3.ReducerQ3.class); 
 		job.setNumReduceTasks(1); 
 		job.setMapOutputKeyClass(Text.class); 
-		job.setMapOutputValueClass(NullWritable.class);
-		job.setOutputKeyClass(IntWritable.class);  
-		job.setOutputValueClass(NullWritable.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		job.setOutputKeyClass(Text.class);  
+		job.setOutputValueClass(DoubleWritable.class);
 		FileInputFormat.addInputPath(job, new Path(args[1]));
 		FileOutputFormat.setOutputPath(job, new Path(args[2]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
