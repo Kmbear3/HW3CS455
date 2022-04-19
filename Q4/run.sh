843167e8309682761ad73f67e94bc5e6ea162c42
Kass <kmbarram@gmail.com>
2022-04-19 11:12:47 -0600

    change run.sh in q3 and q4

--- Q4/run.sh
+++ Q4/run.sh
@@ -1,5 +1,11 @@
+#! /bin/bash
+
 clear
 
 gradle build
 
-$HADOOP_HOME/bin/hadoop jar build/libs/Q4-1.0-SNAPSHOT.jar Question4 /hw3_input /q4_0
+pack_name=output_q4_$(date +%s)
+
+$HADOOP_HOME/bin/hadoop jar build/libs/Q4-1.0-SNAPSHOT.jar Question4 /hw3_input /"$pack_name"
+
+$HADOOP_HOME/bin/hadoop fs -get /"$pack_name" ./"$pack_name"
